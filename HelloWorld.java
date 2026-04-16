f90c26be927f51bac1c1bde6379205aa57302a66
Himanshu garg <himanshugarg@Himanshus-MacBook-Air-9.local>
2026-04-16 10:01:02 +0530

    UC7: use String.join() for multiple names

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,15 +4,7 @@ public class HelloWorld {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String result = "";
-
-            for (String name : args) {
-                result += name + ", ";
-            }
-
-            // remove last ", "
-            result = result.substring(0, result.length() - 2);
-
+            String result = String.join(", ", args);
             System.out.println("Hello, " + result + "!");
         }
 
